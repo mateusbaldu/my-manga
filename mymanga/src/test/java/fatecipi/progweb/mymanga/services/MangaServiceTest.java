d752b4cba869cb6785848fc4065684e9bd12f588
Mateus Balduino <balduinomateus0@gmail.com>
2025-10-08 15:28:05 -0300

    test: teste unitário do método save
    feitos os dois casos de teste unitário do método de salvar mangá da classe de serviço

--- mymanga/src/test/java/fatecipi/progweb/mymanga/services/MangaServiceTest.java
+++ mymanga/src/test/java/fatecipi/progweb/mymanga/services/MangaServiceTest.java
@@ -4,6 +4,7 @@ import fatecipi.progweb.mymanga.configs.mappers.MangaMapper;
 import fatecipi.progweb.mymanga.configs.mappers.VolumeMapper;
 import fatecipi.progweb.mymanga.enums.Genres;
 import fatecipi.progweb.mymanga.enums.MangaStatus;
+import fatecipi.progweb.mymanga.exceptions.ResourceAlreadyExistsException;
 import fatecipi.progweb.mymanga.exceptions.ResourceNotFoundException;
 import fatecipi.progweb.mymanga.models.Manga;
 import fatecipi.progweb.mymanga.models.dto.manga.MangaCreateAndUpdate;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -42,6 +45,13 @@ class MangaServiceTest {
     private VolumeMapper volumeMapper;
     @InjectMocks
     private MangaService mangaService;
+
+    @Captor
+    private ArgumentCaptor<MangaCreateAndUpdate> mangaCreateAndUpdateCaptor;
+    @Captor
+    private ArgumentCaptor<Manga> mangaCaptor;
+    @Captor
+    private ArgumentCaptor<MangaResponse> mangaResponseCaptor;
 
     @Nested
     class listAll {
@@ -141,7 +151,7 @@ class MangaServiceTest {
                     Genres.ACTION,
                     null
             );
-            when(mangaMapper.toMangaResponseDto(manga)).thenReturn(mangaResponse);
+            when(mangaMapper.toMangaResponseDto(any(Manga.class))).thenReturn(mangaResponse);
             when(mangaRepository.findById(1L)).thenReturn(Optional.of(manga));
 
             //Act
@@ -149,9 +159,12 @@ class MangaServiceTest {
 
             //Assert
             assertNotNull(output);
+            verify(mangaMapper, times(1)).toMangaResponseDto(mangaCaptor.capture());
             verify(mangaRepository, atLeastOnce()).findById(1L);
-            assertEquals(mangaResponse.id(), output.id());
-            assertEquals(mangaResponse.title(), output.title());
+
+            Manga capturedManga = mangaCaptor.getValue();
+            assertEquals(capturedManga.getId(), output.id());
+            assertEquals(capturedManga.getTitle(), output.title());
         }
         @Test
         @DisplayName("should throw a ResourceNotFoundException when the Mangá is not found")
@@ -201,6 +214,7 @@ class MangaServiceTest {
 
             assertNotNull(output);
             assertEquals(1, output.getTotalElements());
+            verify(mangaMapper, times(1)).toMangaResponseDto(any(Manga.class));
             verify(mangaRepository, atLeastOnce()).findByKeyword("test", pageable);
         }
     }
@@ -228,6 +242,7 @@ class MangaServiceTest {
             mangaService.deleteById(mangaId);
 
             verify(mangaRepository, times(1)).delete(manga);
+            verify(mangaRepository, times(1)).findById(mangaId);
         }
 
         @Test
@@ -309,6 +324,75 @@ class MangaServiceTest {
             //Act & Assert
             assertThrows(ResourceNotFoundException.class, () -> mangaService.update(1L, mangaUpdate));
             verify(mangaRepository, times(1)).findById(1L);
+        }
+    }
+
+    @Nested
+    class save {
+        @Test
+        @DisplayName("should save a Mangá successfully when everything is ok")
+        void save_returnMangaResponse_whenEverythingIsOK() {
+            MangaCreateAndUpdate newManga = new MangaCreateAndUpdate(
+                    "Test",
+                    "Test Author",
+                    "Test Mangá test test",
+                    1.5,
+                    MangaStatus.COMPLETED,
+                    Genres.ACTION,
+                    null
+            );
+            Manga mappedManga = new Manga(
+                    1L,
+                    "Test",
+                    "Test Author",
+                    "Test Mangá test test",
+                    1.5,
+                    "Test",
+                    MangaStatus.COMPLETED,
+                    Genres.ACTION,
+                    null
+            );
+            Manga m = new Manga();
+            MangaResponse mangaResponse = new MangaResponse(
+                    1L,
+                    "Test",
+                    "Test Author",
+                    "Test Mangá test test",
+                    1.5,
+                    "Test",
+                    MangaStatus.COMPLETED,
+                    Genres.ACTION,
+                    null
+            );
+            doReturn(false).when(mangaRepository).existsByTitle(anyString());
+            doNothing().when(mangaMapper).mapManga(any(), any());
+            doReturn(mappedManga).when(mangaRepository).save(any());
+            doReturn(mangaResponse).when(mangaMapper).toMangaResponseDto(any());
+
+            MangaResponse output = mangaService.save(newManga);
+
+            assertNotNull(output);
+            verify(mangaRepository, times(1)).existsByTitle(anyString());
+            verify(mangaRepository, times(1)).save(any());
+            verify(mangaMapper, times(1)).mapManga(any(), any());
+            assertEquals(newManga.title(), output.title());
+        }
+
+        @Test
+        @DisplayName("should throw a exception when the Mangá already exists")
+        void save_throwException_whenTheMangaAlreadyExists() {
+            MangaCreateAndUpdate newManga = new MangaCreateAndUpdate(
+                    "Test",
+                    "Test Author",
+                    "Test Mangá test test",
+                    1.5,
+                    MangaStatus.COMPLETED,
+                    Genres.ACTION,
+                    null
+            );
+            doReturn(true).when(mangaRepository).existsByTitle(anyString());
+
+            assertThrows(ResourceAlreadyExistsException.class, () -> mangaService.save(newManga));
         }
     }
 }
