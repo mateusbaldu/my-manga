9469ad1425c1a4d93a1bc647543ee63216325344
Mateus Balduino <balduinomateus0@gmail.com>
2026-06-23 17:31:57 -0300

    feat(api): adiciona biblioteca do Redis, configurações básicas no application.properties e habilita cache na aplicação

--- mymanga/src/main/java/fatecipi/progweb/mymanga/MymangaApplication.java
+++ mymanga/src/main/java/fatecipi/progweb/mymanga/MymangaApplication.java
@@ -2,10 +2,12 @@ package fatecipi.progweb.mymanga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCaching
 public class MymangaApplication {
 
 	public static void main(String[] args) {
